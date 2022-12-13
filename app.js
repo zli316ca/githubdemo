418fea244dd3c83b15d31622a1170f101168c131
zli316 <zli316@myseneca.ca>
2022-12-12 23:00:32 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+
+
+
+app.get("/", (req, res) => {
+
+   
+    res.send("Welcome github Page!!!")
+
+ 
+})
+
+app.listen(2500, () => {
+    console.log("<-----------App is Listening at Port 2500!!!!!!!!!!!!!!!!!!!--->")
+})
