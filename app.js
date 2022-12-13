fabcc77cd86ff5e1e422fde9c9677d53eecf70f5
zli316 <zli316@myseneca.ca>
2022-12-13 00:20:02 -0500

    add delcaration

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+/********************************************************************************* 
+*  WEB322 – Assignment 6 
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.   
+*  No part of this assignment has been copied manually or electronically from any other source 
+*  (including web sites) or distributed to other students. 
+*  
+*  Name: ______zhongyao Li_ Student ID: ____153743216__________ Date: _____2022 /12/12___________ 
+* 
+*  Online (Cyclic) URL: _____https://enthusiastic-waistcoat-dove.cyclic.app/__________________________________________________ 
+* 
+********************************************************************************/  
+
+
+
 const express = require('express')
 
 const app = express()
